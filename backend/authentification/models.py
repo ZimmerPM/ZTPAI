7ff73bf11cc82862a7528e24d28b9ed76f9b63ca
Piotr <p.zimirski@gmail.com>
2023-12-22 12:19:47 +0100

    Changes in files responsible for databases

--- backend/authentification/models.py
+++ backend/authentification/models.py
@@ -1,3 +1,48 @@
+#from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email, password=None, **extra_fields):
+#         if not email:
+#             raise ValueError('The Email must be set')
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+#
+#     def create_superuser(self, email, password, **extra_fields):
+#         extra_fields.setdefault('is_staff', True)
+#         extra_fields.setdefault('is_superuser', True)
+#
+#         if extra_fields.get('is_staff') is not True:
+#             raise ValueError('Superuser must have is_staff=True.')
+#         if extra_fields.get('is_superuser') is not True:
+#             raise ValueError('Superuser must have is_superuser=True.')
+#
+#         return self.create_user(email, password, **extra_fields)
+#
+# class CustomUser(AbstractBaseUser, PermissionsMixin):
+#     email = models.EmailField(unique=True, max_length=255)
+#     password = models.CharField(max_length=255)
+#     role = models.CharField(max_length=255)
+#     is_active = models.BooleanField(default=True)
+#     is_staff = models.BooleanField(default=False)
+#
+#     objects = CustomUserManager()
+#
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+#
+#     def __str__(self):
+#         return self.email
+#
+# class UserDetails(models.Model):
+#     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='details')
+#     name = models.CharField(max_length=255)
+#     lastname = models.CharField(max_length=255)
+#
+#     def __str__(self):
+#         return f"{self.name} {self.lastname}"
